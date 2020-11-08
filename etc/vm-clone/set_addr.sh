cd76a45c8e8e05e1b489aa6dc1d3ecd8ae9a4e19
SedaikoStumchek <root@test-app1.home.lan>
2020-11-08 11:39:59 +1100

    -

--- etc/vm-clone/set_addr.sh
+++ etc/vm-clone/set_addr.sh
@@ -12,6 +12,11 @@ IF_CONF=/etc/sysconfig/network-scripts/ifcfg-$DEV
 #Текущий MAC адрес 
 CUR_MAC=$(ip a show $DEV | head -n2 | tail -n1 | awk '{print $2}')
 
+if [[ -z "$CUR_MAC" ]] ; then
+	echo "Can't determine MAC address" >&2
+	exit 1
+fi
+
 #Текущий IPv4
 CUR_IP=$(cat $IF_CONF | grep 'IPADDR=' | cut -d'"' -f2)
 
@@ -21,6 +26,8 @@ CUR_HOST=$(hostname)
 #Если MAC есть в файле, то это продакшин
 if grep -q "$CUR_MAC" $MAC_LIST
 then
+	TYPE=$(grep $CUR_MAC $MAC_LIST | xargs | cut -d' ' -f1)
+	echo "This is prodaction server type '$TYPE'"
     exit 0
 fi
 
@@ -55,7 +62,31 @@ fi
 if [ $TYPE == "app-cs" ]
 then
     rm -rf /home/usr1cv8/.1cv8
-    echo "remove cluster 1C"	
+    echo "Remove cluster 1C"	
     rm -rf /var/1C/license/*.*
-    echo "remove licenses 1C"	
+    echo "Remove licenses 1C"	
+fi
+
+if [ $TYPE == "db-srv" ]
+then
+	PGDATA=/mnt/db_disk/pg_data
+	
+	APP_IP=$(grep -E "app-cs .* clone " $MAC_LIST | xargs | cut -d' ' -f3)
+	OLD_APP_IP=$(grep -E "app-cs " $MAC_LIST | grep -v 'clone' | xargs | cut -d' ' -f3)
+    
+	sed -i "s/$OLD_APP_IP/$APP_IP/" $PGDATA/pg_hba.conf
+    
+	echo "Replace IP $OLD_APP_IP->$APP_IP in pg_hba.conf"    	
+fi
+
+if [ $TYPE == "web-srv" ]
+then
+	WEB_DIR=/var/www/1c
+	
+	APP_HOST=$(grep -E "app-cs .* clone " $MAC_LIST | xargs | cut -d' ' -f4)
+	OLD_APP_HOST=$(grep -E "app-cs " $MAC_LIST | grep -v 'clone' | xargs | cut -d' ' -f4)
+    
+	find $WEB_DIR -type f -exec sed -i "s/$OLD_APP_HOST/$APP_HOST/g" {} \+
+    
+	echo "Replace hostname $OLD_APP_HOST->$APP_HOST in $WEB_DIR/*/*.vrd"    	
 fi
